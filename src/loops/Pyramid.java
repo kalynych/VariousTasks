d3b2f5583e0a20da602ee16ffdc83b5fb5dfda9a
T. E <kalynych017@gmail.com>
2022-07-03 02:01:38 -0400

    pyramid

---
+++ src/loops/Pyramid.java
@@ -0,0 +1,27 @@
+package loops;
+
+public class Pyramid {
+
+    public static void main(String[] args) {
+
+        int maxI = 6;
+        String space = " ";
+
+        for (int i = 0; i < maxI; i++){
+            System.out.print(space.repeat((maxI - i)*2));
+
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(space);
+                System.out.print((i - j));
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(space);
+                System.out.print((j));
+            }
+
+            System.out.println();
+        }
+
+    }
+}
